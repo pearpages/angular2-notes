6c3439468da86599b6dee27b325f6eaa8bc2fa57
pearpages <pere@soms.cat>
2016-08-24 18:25:53 +0200

    hello world app

--- examples2/minimal/src/main.ts
+++ examples2/minimal/src/main.ts
@@ -1,8 +1,8 @@
-/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
+/// <reference path="../../node_modules/angular2/typings/browser.d.ts" />
 
 import 'reflect-metadata';
-import '../node_modules/es6-shim/es6-shim.js';
-import '../node_modules/zone.js/dist/zone.js';
+import '../../node_modules/es6-shim/es6-shim.js';
+import '../../node_modules/zone.js/dist/zone.js';
 import {bootstrap} from 'angular2/platform/browser';
 import {App} from './app';
 
