98ccb1301669b82b701136c137793905c5b12372
pearpages <pere@soms.cat>
2016-08-25 23:48:50 +0200

    observables

--- examples2/minimal/src/app.ts
+++ examples2/minimal/src/app.ts
@@ -1,11 +1,15 @@
 import {Component} from 'angular2/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
 
 @Component({
     selector: 'app',
     template: `
     <h1>{{message}}</h1>
+    <p>Clock: {{clock | async}}</p>
     ` 
 })
 export class App {
     message = 'Hello, world';
+    clock = Observable.interval(1000);
 }
