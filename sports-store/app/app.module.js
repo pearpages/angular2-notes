b5fd3bb9bd9611e311f7ca80144977cbb8230cf1
pearpages <pere@soms.cat>
2017-05-29 10:25:55 +0200

    route guards

--- sports-store/app/app.module.js
+++ sports-store/app/app.module.js
@@ -16,17 +16,28 @@ var store_component_1 = require("./store/store.component");
 var checkout_component_1 = require("./store/checkout.component");
 var cartDetail_component_1 = require("./store/cartDetail.component");
 var router_1 = require("@angular/router");
+var storeFirst_guard_1 = require("./storeFirst.guard");
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
             imports: [platform_browser_1.BrowserModule, store_module_1.StoreModule, router_1.RouterModule.forRoot([
-                    { path: "store", component: store_component_1.StoreComponent },
-                    { path: "cart", component: cartDetail_component_1.CartDetailComponent },
-                    { path: "checkout", component: checkout_component_1.CheckoutComponent },
+                    {
+                        path: "store", component: store_component_1.StoreComponent,
+                        canActivate: [storeFirst_guard_1.StoreFirstGuard]
+                    },
+                    {
+                        path: "cart", component: cartDetail_component_1.CartDetailComponent,
+                        canActivate: [storeFirst_guard_1.StoreFirstGuard]
+                    },
+                    {
+                        path: "checkout", component: checkout_component_1.CheckoutComponent,
+                        canActivate: [storeFirst_guard_1.StoreFirstGuard]
+                    },
                     { path: "**", redirectTo: "/store" }
                 ])],
+            providers: [storeFirst_guard_1.StoreFirstGuard],
             declarations: [app_component_1.AppComponent],
             bootstrap: [app_component_1.AppComponent]
         }), 
--- sports-store/app/app.module.ts
+++ sports-store/app/app.module.ts
@@ -6,14 +6,25 @@ import { StoreComponent } from "./store/store.component";
 import { CheckoutComponent } from "./store/checkout.component";
 import { CartDetailComponent } from "./store/cartDetail.component";
 import { RouterModule } from "@angular/router";
+import { StoreFirstGuard } from "./storeFirst.guard";
 
 @NgModule({
     imports: [BrowserModule, StoreModule, RouterModule.forRoot([
-        { path: "store", component: StoreComponent },
-        { path: "cart", component: CartDetailComponent },
-        { path: "checkout", component: CheckoutComponent },
+        {
+            path: "store", component: StoreComponent,
+            canActivate: [StoreFirstGuard]
+        },
+        {
+            path: "cart", component: CartDetailComponent,
+            canActivate: [StoreFirstGuard]
+        },
+        {
+            path: "checkout", component: CheckoutComponent,
+            canActivate: [StoreFirstGuard]
+        },
         { path: "**", redirectTo: "/store" }
     ])],
+    providers: [StoreFirstGuard],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
