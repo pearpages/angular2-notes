0ad7941469b4639e9c21881e72561b163fbc913c
pearpages <pere@soms.cat>
2017-05-30 23:06:13 +0200

    template driven

--- lists/src/app/my-forms/first-form/first-form.component.ts
+++ lists/src/app/my-forms/first-form/first-form.component.ts
@@ -1,5 +1,9 @@
-import { FormBuilder } from '@angular/forms';
+import { Hero } from './hero';
 import { Component, OnInit } from '@angular/core';
+
+// let myHero = new Hero(42, 'SkyDog',
+//   'Fetch any object at any distance',
+//   'Leslie Rollover');
 
 @Component({
   selector: 'app-first-form',
@@ -8,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstFormComponent implements OnInit {
 
-  constructor(private _builder: FormBuilder) { }
+  powers = ['Really Smart', 'Super Flexible',
+    'Super Hot', 'Weather Changer'];
 
-  ngOnInit() {
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  modelSubmitted;
+
+  onSubmit() {
+    this.submitted = true;
+    this.modelSubmitted = Object.assign({},this.model);
   }
 
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+
+  touched(value) {
+    return value.split(' ').indexOf('ng-touched') !== -1;
+  }
+  pristine(value) {
+    return value.split(' ').indexOf('ng-pristine') !== -1;
+  }
+  valid(value) {
+    return value.split(' ').indexOf('ng-valid') !== -1;
+  }
 }
