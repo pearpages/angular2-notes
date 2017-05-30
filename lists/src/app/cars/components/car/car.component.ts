619d7c0cc528a188286aa24a923b26145c11c73d
pearpages <pere@soms.cat>
2017-05-30 16:44:01 +0200

    routes navigation

--- lists/src/app/cars/components/car/car.component.ts
+++ lists/src/app/cars/components/car/car.component.ts
@@ -1,5 +1,7 @@
 import { Car } from './../../models/Car';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-car',
@@ -10,13 +12,38 @@ export class CarComponent implements OnInit {
 
   @Input() car: Car;
   @Output() carClick: EventEmitter<string> = new EventEmitter();
+  static times = 0;
+  id: number;
 
-  constructor() { }
+  get times() {
+    return CarComponent.times;
+  }
+
+  constructor(public route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe( (params: Params) => this.id = params.id);
+    // example
+    // this.route.params
+    //   // (+) converts string 'id' to a number
+    //   .switchMap((params: Params) => this.service.getHero(+params['id']))
+    //   .subscribe((hero: Hero) => this.hero = hero);
+
+    // snapshot is the nonOvserbable version
+      // (+) converts string 'id' to a number
+    // let id = +this.route.snapshot.params['id'];
+
+    // The switchMap operator allows you to perform an action with the current value of the Observable, and map it to a new Observable.
+
+    // When subscribing to an observable in a component, you almost always arrange to unsubscribe when the component is destroyed.
+
+    // There are a few exceptional observables where this is not necessary. The ActivatedRoute observables are among the exceptions.
   }
 
   performClick($event) {
+    console.log('event emitted from CarComponent');
+    CarComponent.times++;
     this.carClick.emit($event.target.value);
   }
 
--- lists/src/app/cars/components/cars-panel/cars-panel.component.ts
+++ lists/src/app/cars/components/cars-panel/cars-panel.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsPanelComponent implements OnInit {
 
+  constructor(private _router: Router) {}
+
   ngOnInit() {}
 
   handleCarClick(id) {
     console.log(id);
+  }
+
+  gotoCar(id: number) {
+    this._router.navigate(['/car',id]);
   }
 }
