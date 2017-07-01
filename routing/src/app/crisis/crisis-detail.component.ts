38d935a34b8bc179820fa33af4f94959deffa277
pearpages <pere@soms.cat>
2017-07-01 15:44:00 +0200

    relative paths

--- routing/src/app/crisis/crisis-detail.component.ts
+++ routing/src/app/crisis/crisis-detail.component.ts
@@ -2,12 +2,13 @@ import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/map';
 
 @Component({
     selector: 'ro-crisis-detail',
     template: `
     <div>
-        <h3>Detail</h3>
+        <h3>Detail {{id}}</h3>
         <button (click)="goBackToCrises()">Go Back To Crises</button>
     </div>
     `
@@ -15,18 +16,19 @@ import 'rxjs/add/operator/take';
 
 export class CrisisDetailComponent implements OnInit {
 
-    id$: Observable<string>;
+    id: string;
 
     constructor(private route: ActivatedRoute, private router: Router) { }
 
     ngOnInit() {
-        this.id$ = this.route.params.map((params: ParamMap) => params.get('id'));
+        // "The Router manages the observables it provides and localizes the subscriptions. The subscriptions are cleaned up when the component is destroyed, protecting against memory leaks, so we don't need to unsubscribe from the route params Observable."
+        this.route.paramMap
+            .map((params: ParamMap) => params.get('id'))
+            .subscribe(id => this.id = id);
     }
 
     goBackToCrises() {
-        this.id$.take(1).map( (id: string) =>
-            // Relative navigation back to the crises
-            this.router.navigate(['../', id], { relativeTo: this.route })
-        );
+        // Relative navigation back to the crises
+        this.router.navigate(['../', {id: this.id, foo: 'foo'}], { relativeTo: this.route })
     }
 }
