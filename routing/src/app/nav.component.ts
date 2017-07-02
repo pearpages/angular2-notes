7854d4a7d6aeac9eaa6e7d589c7620b0d23b031f
pearpages <pere@soms.cat>
2017-07-02 22:44:08 +0200

    navigation

--- routing/src/app/nav.component.ts
+++ routing/src/app/nav.component.ts
@@ -5,29 +5,51 @@ import { Router } from '@angular/router';
     selector: 'ro-nav',
     template: `
         <nav>
-            <a routerLinkActive="active" routerLink="root">Home</a>
-            <a routerLinkActive="active" routerLink="login">Login</a>
-            <a routerLinkActive="active" routerLink="admin">Admin</a>
-            <a routerLinkActive="active" routerLink="admin/crises">Manage Crises</a>
-            <a routerLinkActive="active" routerLink="crisis-center">Crisis</a>
-            <a routerLinkActive="active" routerLink="blue" [routerLinkActiveOptions]="{ exact: true }">Blue</a>
-            <a routerLinkActive="active" routerLink="blue/house">Blue</a>
-            <a routerLinkActive="active"
-            routerLink="blue"
-            [queryParams]="{color:'red', name:'Pere'}" [fragment]="'my-history'">Blue queryParams</a>
-            <a routerLinkActive="active" routerLink="red">Red</a>
-            <a routerLinkActive="active" routerLink="orange">Orange</a>
-            <a routerLinkActive="active" routerLink="black">Redirection Example</a>
-            <a routerLinkActive="active" routerLink="black/car">Redirection Example 2</a>
-            <a routerLinkActive="active" routerLink="brown">Redirection Example 3</a>
-            <a routerLinkActive="active" routerLink="hero/hello">Precedence Example</a>
-            <a routerLinkActive="active" routerLink="hero/joan">Hero Joan</a>
-            <a routerLinkActive="active" routerLink="heroes">Heroes List</a>
+            <div>
+                <strong>AppModule</strong>
+                <a routerLinkActive="active" routerLink="root">Home</a>
+            </div>
+            <div>
+                <strong>LoginModule</strong>
+                <a routerLinkActive="active" routerLink="login">Login</a>
+            </div>
+            <div>
+                <strong>AdminModule</strong>
+                <a routerLinkActive="active" routerLink="admin">Admin</a>
+                <a routerLinkActive="active" routerLink="admin/crises">Manage Crises</a>
+            </div>
+            <div>
+                <strong>CrisisModule</strong>
+                <a routerLinkActive="active" routerLink="crisis-center">Crisis</a>
+            </div>
+            <div>
+                <strong>ColorsModule</strong>
+                <a routerLinkActive="active" routerLink="blue" [routerLinkActiveOptions]="{ exact: true }">Blue</a>
+                <a routerLinkActive="active" routerLink="blue/house">Blue</a>
+                <a routerLinkActive="active"
+                routerLink="blue"
+                [queryParams]="{color:'red', name:'Pere'}" [fragment]="'my-history'">Blue queryParams</a>
+                <a routerLinkActive="active" routerLink="red">Red</a>
+                <a routerLinkActive="active" routerLink="orange">Orange</a>
+                <a routerLinkActive="active" routerLink="black">Redirection Example</a>
+                <a routerLinkActive="active" routerLink="black/car">Redirection Example 2</a>
+                <a routerLinkActive="active" routerLink="brown">Redirection Example 3</a>
+            </div>
+            <div>
+                <strong>HeroesModule</strong>
+                <a routerLinkActive="active" routerLink="hero/hello">Precedence Example</a>
+                <a routerLinkActive="active" routerLink="hero/joan">Hero Joan</a>
+                <a routerLinkActive="active" routerLink="heroes">Heroes List</a>
+            </div>
         </nav>
-        <button (click)="gotoHeroes()">heroes</button>
-        <button (click)="gotoHeroes({id: 'carles', var2: 'whatever2'})">heroes with params (Matrix URL)</button>
-        <button (click)="gotoHero('pere')">hero pere</button>
-        <button (click)="gotoHero('carles','soler')">hero carles soler</button>
+        <hr/>
+        <div class="buttons">
+            <button (click)="gotoHeroes()">heroes</button>
+            <button (click)="gotoHeroes({id: 'carles', var2: 'whatever2'})">heroes with params (Matrix URL)</button>
+            <button (click)="gotoHero('pere')">hero pere</button>
+            <button (click)="gotoHero('carles','soler')">hero carles soler</button>
+        </div>
+        <hr/>
         `,
     styles: [`
         a.active {
