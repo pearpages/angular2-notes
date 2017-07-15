a0a8344e408f0a1bda3adebeaf80a1d23a60f1c5
Pere Pages <pere.pages@weareblueorange.com>
2017-07-15 15:46:41 +0200

    reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { StockInventoryModule } from './stock-inventory/stock-inventory.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StockInventoryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/stock-inventory/containers/stock-inventory.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'no-stock-inventory',
+  template: `HELLO WORLD`
+})
+
+export class StockInventoryComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
