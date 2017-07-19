e763144f9e78acfdad37121016f5cacd279ce6a4
Pere Pages <pere.pages@weareblueorange.com>
2017-07-19 16:08:00 +0200

    reactive forms

---
+++ docs/15-reactive-forms/02-formcontrols-and-formgroups.md
@@ -0,0 +1,57 @@
+# Componentizing FormGroups
+
+This is the real first form example.
+
+```typescript
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'no-stock-inventory',
+  template: `
+  <div class="no-stock-inventory">
+    <form [formGroup]="form" (ngSubmit)="onSubmit()">
+
+      <div formGroupName="store">
+        <input 
+          type="text"
+          placeholder="Branch ID"
+          formControlName="branch"
+        >
+        <input 
+          type="text"
+          placeholder="Manager Code"
+          formControlName="code"
+        >
+      </div>
+
+      <div class="no-stock-inventory__buttons">
+        <button 
+          type="submit"
+          [disabled]="form.invalid"
+        >Order stock</button>
+      </div>
+
+      <pre>{{ form.value | json }}</pre>
+
+    </form>
+  </div>
+  `,
+  styles: [`
+  `]
+})
+
+export class StockInventoryComponent {
+  // we use formGroupName's and formControlName's
+  form = new FormGroup({
+    store: new FormGroup({
+      branch: new FormControl('B182'), // <-- default value
+      code: new FormControl('1234')
+    })
+  });
+
+  onSubmit() {
+    console.log('submit', this.form.value);
+  }
+}
+```
--- src/app/stock-inventory/containers/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory.component.ts
@@ -1,18 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'no-stock-inventory',
   template: `
   <div class="no-stock-inventory">
-    HELLO WORLD
+    <form [formGroup]="form" (ngSubmit)="onSubmit()">
+
+      <div formGroupName="store">
+        <input 
+          type="text"
+          placeholder="Branch ID"
+          formControlName="branch"
+        >
+        <input 
+          type="text"
+          placeholder="Manager Code"
+          formControlName="code"
+        >
+      </div>
+
+      <div class="no-stock-inventory__buttons">
+        <button 
+          type="submit"
+          [disabled]="form.invalid"
+        >Order stock</button>
+      </div>
+
+      <pre>{{ form.value | json }}</pre>
+
+    </form>
   </div>
   `,
   styles: [`
   `]
 })
 
-export class StockInventoryComponent implements OnInit {
-  constructor() { }
+export class StockInventoryComponent {
+  form = new FormGroup({
+    store: new FormGroup({
+      branch: new FormControl('B182'), // <-- default value
+      code: new FormControl('1234')
+    })
+  });
 
-  ngOnInit() { }
+  onSubmit() {
+    console.log('submit', this.form.value);
+  }
 }
