c0b6c3ac788d625a8c6b6b97106ca088deed31b5
Pere Pages <pere.pages@weareblueorange.com>
2017-08-18 12:17:06 +0200

    styles

--- src/app/stock-inventory/components/stock-branch.component.ts
+++ src/app/stock-inventory/components/stock-branch.component.ts
@@ -4,15 +4,20 @@ import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'no-stock-branch',
   template: `
-  <div [formGroup]="parent">
+  <div [formGroup]="parent" class="no-stock-branch">
+    <h4>Stock Branch</h4>
     <div formGroupName="store">
+      <label for="branch">Branch ID</label>
       <input
         type="text"
+        id="branch"
         placeholder="Branch ID"
         formControlName="branch"
       >
+      <label for="code">Manager Code</label>
       <input
         type="text"
+        id="code"
         placeholder="Manager Code"
         formControlName="code"
       >
@@ -20,6 +25,9 @@ import { FormGroup } from '@angular/forms';
   </div>
   `,
   styles: [`
+  .no-stock-branch {
+    margin-bottom: 40px;
+  }
   `]
 })
 
